bf00457a4ef0228f7de490e467e019b7ccbdca45
Johannes Keller <jojok1342@gmail.com>
2021-04-27 10:59:19 +0200

    - entfernen von GETs - unauthorisierte Anfrage an /sensoractive_gateway ist möglich

--- pythonserver.py
+++ pythonserver.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse, parse_qs
 import json
 from colorama import init, Fore, Style
 import os
+from io import BytesIO
 
 import globalvars
 
@@ -23,66 +24,34 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
             'WWW-Authenticate', 'Basic realm="Demo Realm"')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-
-    def do_GET(self):
-        key = self.server.get_auth_key()
-
-        ''' Present frontpage with user authentication. '''
-        if self.headers.get('Authorization') == None:
-            self.do_AUTHHEAD()
-
-            response = {
-                'success': False,
-                'error': 'No auth header received'
-            }
-
-            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
-
-        elif self.headers.get('Authorization') == 'Basic ' + str(key):
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-
-            getvars = self._parse_GET()
-
-            response = {
-                'path': self.path,
-                'get_vars': str(getvars)
-            }
-
-            base_path = urlparse(self.path).path
-            if base_path == '/status':
-                with open(globalvars.path_to_framework_data_json, 'r') as f:
-                    json_data = json.load(f)
-                self.wfile.write(bytes(json.dumps(json_data), 'utf-8'))
-                # Do some work
-                pass
-            elif base_path == '/path2':
-                # Do some work
-                pass
-
-            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
-        else:
-            self.do_AUTHHEAD()
-
-            response = {
-                'success': False,
-                'error': 'Invalid credentials'
-            }
-
-            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+    
+    def get_Postvar(self, x, befehl):
+        for y in x:
+            if y[0] == befehl:
+                return y[1]
+        
+        return "nix"
 
     def do_POST(self):
         key = self.server.get_auth_key()
-
-        ''' Present frontpage with user authentication. '''
-        if self.headers.get('Authorization') == None:
-            self.do_AUTHHEAD()
+        base_path = urlparse(self.path).path        
+        self.do_AUTHHEAD()
+        
+        if self.headers.get('Authorization') == None:  
+            
+            if base_path == '/sensoractive_gateway':
                 
-            response = {
-                'success': False,
-                'error': 'No auth header received'
-            }
+                response = {
+                    'success': True,
+                }
+                
+            else:
+            
+                response = {
+                    'success': False,
+                    'error': 'no auth'
+                }
+            
 
             self.wfile.write(bytes(json.dumps(response), 'utf-8'))
 
@@ -102,27 +71,23 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
             print(p)          
             
             
-            
-            
-            response = {
-                'path': self.path,
-                #'get_vars': str(getvars)
-                #'get_vars': str(postvars)
-            }
-            
-            switch (urlparse(self.path).path) {
-            case '/changeintervall':
-                monthString = "January";
-                break;
-            default: monthString = "Invalid month";
-                     break;
-        }
-            if base_path == '/changeintervall':
+            if base_path == '/status':
                 
+                with open(globalvars.path_to_framework_data_json, 'r') as f:
+                    json_data = json.load(f)
                 
-                """
-                sensor = postvars["sensor"][0]
-                intervall = postvars["intervall"][0]
+                response = {
+                            'success': True,
+                            'error': json_data
+                        }
+                self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+                
+                pass
+            
+            elif base_path == '/changeintervall':
+                
+                sensor = self.get_Postvar(p, "sensor")
+                intervall = self.get_Postvar(p, "intervall")
             
                 print(sensor)
                 print(intervall)
@@ -134,7 +99,7 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
 
                 with open(globalvars.path_to_framework_data_json, "w") as jsonFile:
                     json.dump(data, jsonFile)
-                """
+                
                 # Do some work
                 pass
             elif base_path == '/changeusername':
@@ -172,7 +137,7 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
                 # Do some work
                 pass
             
-            self.wfile.write(bytes(json.dumps(response), 'utf-8'))
+            #self.wfile.write(bytes(json.dumps(response), 'utf-8'))
             
         else:
             self.do_AUTHHEAD()
@@ -186,31 +151,11 @@ class CustomServerHandler(http.server.BaseHTTPRequestHandler):
 
         response = {
             'path': self.path,
-            'get_vars': str(postvars)
+            #'get_vars': str(postvars)
             #'get_vars': str(postvars)
         }
 
         #self.wfile.write(bytes(json.dumps(response), 'utf-8'))
-        
-    def _parse_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-        if ctype == 'multipart/form-data':
-            postvars = cgi.parse_multipart(self.rfile, pdict)
-        elif ctype == 'application/x-www-form-urlencoded':
-            length = int(self.headers['content-length'])
-            postvars = parse_qs(self.rfile.read(length),keep_blank_values=1)
-        else:
-            postvars = {}
-        return postvars
-
-    def _parse_GET(self):
-        print(parse_qs(urlparse(self.path).query))
-        print(urlparse(self.path).query)
-        print(urlparse(self.path))
-        print(self.path)
-        getvars = parse_qs(urlparse(self.path).query)
-
-        return getvars
 
 
 class CustomHTTPServer(http.server.HTTPServer):
